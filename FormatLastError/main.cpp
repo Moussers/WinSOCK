0709d69e568e963c55db13a88de17a2220ab5f4e
Sand <sand999.com>
2026-06-17 23:09:47 +0300

    Interface MPI in process

--- FormatLastError/main.cpp
+++ FormatLastError/main.cpp
@@ -8,8 +8,8 @@ LPSTR FormatLastError(DWORD dwError, CHAR szBuffer[])
 		NULL, dwError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 		(LPSTR)&lpBuffer, 0, NULL);
 	//FORMAT_MESSAGE_ALLOCATE_BUFFER — просит систему выделить память для буфера автоматически.
-	//В этом случае lpMsgBuf должен быть указателем на void*, 
-	//куда будет записан адрес выделенной памяти.
+	//В этом случае lpMsgBuf должен быть указателем на void*, куда будет записан адрес выделенной
+	//памяти.
 	sprintf(szBuffer, "%i: %s", dwError, lpBuffer);
 	LocalFree(lpBuffer);
 	return szBuffer;
